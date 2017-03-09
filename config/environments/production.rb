bc4113ba6ad969cd18ea767415e61ee624c89135
pattiyaa <pattiyaa@gmail.com>
2017-03-09 19:53:28 +0700

    update image magic path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # Paperclip config:
+  Paperclip.options[:image_magick_path] = "spree"
+  Paperclip.options[:command_path] = "spree/bin"
 end
