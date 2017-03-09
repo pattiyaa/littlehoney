5da6d63a49f33a8a6a7a997a99e8db1164bb1ff2
pattiyaa <pattiyaa@gmail.com>
2017-03-09 13:29:11 +0700

    delete admin deface change title

--- app/override/admin_change_title.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => 'spree/admin/shared/_head',
-                     :name => 'override',
-                     :replace_contents => 'title',
-                     :title => 'MyOnly Administrativo')
