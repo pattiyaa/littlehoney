2c483e85c697feeb58cf41608b5ac4b0fcbf0da9
pattiyaa <pattiyaa@gmail.com>
2017-03-11 21:29:52 +0700

    add admin logo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  # Paperclip config:
-  Paperclip.options[:image_magick_path] = "spree"
-  Paperclip.options[:command_path] = "spree/bin"
+
 end
